030df80e7f9f200edbd1a1dfe6c27310108fda8c
Norbert Świderski <norbert.swiderski@gmail.com>
2020-07-03 13:50:53 +0200

    feat: add toggle function to about button

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
+const aboutButton = document.querySelector('.about_button-js');
+aboutButton.addEventListener('click', ()=>{
+    const aboutText = document.querySelector('.about_paragraphs-js');
+    aboutText.classList.toggle('about_paragraphs_open');
+    if(aboutButton.innerHTML=="Zwiń"){
+        aboutButton.innerHTML="Zobacz więcej";
+    }
+    else{
+        aboutButton.innerHTML="Zwiń";
+    }
+});
+
 const hamburger = document.querySelector('.hamburger--js');
 
 hamburger.addEventListener('click', ()=>{
-    const nav = document.querySelector('.navigation-js')
+    const nav = document.querySelector('.navigation-js');
     nav.classList.toggle('navigation_open');
 });
 
