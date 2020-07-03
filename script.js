77721b6e06af1beeb6a57c44f18506316a82bb2a
Norbert Świderski <norbert.swiderski@gmail.com>
2020-07-03 13:17:18 +0200

    feat: hamburger menu, button 'goTop' added

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', ()=>{
+    const nav = document.querySelector('.navigation-js')
+    nav.classList.toggle('navigation_open');
+});
+
